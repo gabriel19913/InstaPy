8b8dba77e980a04744783cab25c00c9a7a8dd6ab
Gabriel Fonseca <gabrielfonseca91@hotmail.com>
2018-06-06 20:11:25 -0300

    Adding my instagram info, changing quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -6,8 +6,8 @@ from selenium.common.exceptions import NoSuchElementException
 
 from instapy import InstaPy
 
-insta_username = ''
-insta_password = ''
+insta_username = 'grifedocapacete'
+insta_password = 'lucas10'
 
 # set headless_browser=True if you want to run InstaPy on a server
 
@@ -33,12 +33,20 @@ try:
 				     min_followers=45,
 				      min_following=77)
     session.set_do_comment(True, percentage=10)
-    session.set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])
+    session.set_comments(['Que ótima foto!', 'Foto legal!!!', 'Boa foto!', 'Essa foto está linda!', 'Essa imagem é muito boa!', 'Que imagem incrível!', 'Você conseguiu tirar uma bela foto!', 'Parabéns por essa bela foto!'])
     session.set_dont_include(['friend1', 'friend2', 'friend3'])
     session.set_dont_like(['pizza', 'girl'])
 
     # actions
-    session.like_by_tags(['natgeo'], amount=1)
+
+    #session.set_user_interact(amount=5, randomize=True, percentage=100, media='Photo')
+    #session.like_by_locations(['213255513'], amount=10)
+    #session.comment_by_locations(['213255513'], amount=10)
+    session.set_delimit_liking(enabled=True, max=300, min=None)
+    session.set_delimit_commenting(enabled=True, max=None, min=10)
+    session.set_user_interact(amount=2, percentage=70, randomize=True, media='Photo')
+    session.follow_likers (['peelscapacetes' , 'ls2brasil'], photos_grab_amount = 2, follow_likers_per_photo = 3, randomize=True, sleep_delay=600, interact=True)
+    session.like_by_tags(['capacete', 'duasrodas', 'instamoto'], amount=10, interact=True)
 
 except Exception as exc:
     # if changes to IG layout, upload the file to help us locate the change
